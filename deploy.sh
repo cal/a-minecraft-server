9117f84b473b3f12da96e8b398f7a66fe2d71188
Carl Johan Crafoord <mail@carljohancrafoord.se>
2012-05-13 14:37:24 +0200

    Daylight is the smallest plugin ever

--- deploy.sh
+++ deploy.sh
@@ -6,3 +6,6 @@ rm -f target/server/*.jar
 
 # deploy server
 find build/CraftBukkit -name "craftbukkit*.jar" -exec cp "{}" target/server \;
+
+# deploy plugins
+find build -name "*.jar" -not -path "*Bukkit*" -exec cp "{}" target/server/plugins \;
