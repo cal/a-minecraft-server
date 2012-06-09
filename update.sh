8f07ec89132f7e2ca80ec0b2758fd27aae483580
Carl Johan Crafoord <mail@carljohancrafoord.se>
2012-06-09 22:52:45 +0200

    Rebuilding

--- update.sh
+++ update.sh
@@ -5,7 +5,7 @@ rm -f server/plugins/*.jar
 rm -f server/*.jar
 
 # deploy server
-find build/CraftBukkit -name "craftbukkit*.jar" -exec cp "{}" server \;
+find build/CraftBukkit -name "craftbukkit*.jar" -exec cp "{}" server/ \;
 
 # deploy plugins
-find build -name "*.jar" -not -path "*Bukkit*" -exec cp "{}" server/plugins \;
+find build -name "*.jar" -not -path "*Bukkit*" -exec cp "{}" server/plugins/ \;
