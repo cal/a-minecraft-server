853fe7339468aacdb754b652e851d69245cff741
Carl Johan Crafoord <mail@carljohancrafoord.se>
2012-06-09 22:45:20 +0200

    Restarting

--- update.sh
+++ update.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 # clean up
-rm -f target/server/plugins/*.jar
-rm -f target/server/*.jar
+rm -f server/plugins/*.jar
+rm -f server/*.jar
 
 # deploy server
-find build/CraftBukkit -name "craftbukkit*.jar" -exec cp "{}" target/server \;
+find build/CraftBukkit -name "craftbukkit*.jar" -exec cp "{}" server \;
 
 # deploy plugins
-find build -name "*.jar" -not -path "*Bukkit*" -exec cp "{}" target/server/plugins \;
+find build -name "*.jar" -not -path "*Bukkit*" -exec cp "{}" server/plugins \;
